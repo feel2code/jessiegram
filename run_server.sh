9fc3458d9d28290b24fd2566a08772299e6bd088
fnabiullin <fnabiullin@fxcintel.com>
2023-03-22 18:06:36 +0400

    simple script for start server

--- run_server.sh
+++ run_server.sh
@@ -2,7 +2,7 @@
 echo 'Starting Django server...'
 source venv/bin/activate
 cd jessiegram
-python3 manage.py runserver 0:8000 --insecure &
+python3 manage.py runserver 0:80 --insecure &
 deactivate
 echo 'Closing Django server...'
 sleep 10
