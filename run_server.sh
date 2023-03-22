2006a55497b4056811fa6d6418761fc1e33324ff
fnabiullin <fnabiullin@fxcintel.com>
2023-03-23 01:02:56 +0400

    migrate to nginx and gunicorn

--- run_server.sh
+++ run_server.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 echo 'Starting Django server...'
+apt install nginx -y
+ufw allow 'Nginx Full'
+ufw allow OpenSSH
 source venv/bin/activate
 cd jessiegram
-python3 manage.py runserver 0:80 --insecure &
-deactivate
-echo 'Closing Django server...'
-sleep 10
+cat misc/default > /etc/nginx/sites-enabled/default
+cat misc/gunicorn.service > /etc/systemd/system/gunicorn.service
+cat misc/nginx.conf > /etc/nginx/nginx.conf
+systemctl start gunicorn
+systemctl enable gunicorn
+systemctl restart gunicorn
+python jessiegram/manage.py collectstatic --no-input
+systemctl start nginx
+systemctl reload nginx
